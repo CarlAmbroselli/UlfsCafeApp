0eff8c964566d30d65a3469cc1544a88647d98ef
Dustin Gedlich <dustin.gedlich@googlemail.com>
2013-06-06 14:56:49 +0200

    implemented FB Login & Request

--- Ulf/LoginViewController.m
+++ Ulf/LoginViewController.m
@@ -20,6 +20,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+//    [[FBSession activeSession] closeAndClearTokenInformation];
 }
 
 
@@ -28,41 +29,30 @@
     //Help (Maybe): https://developers.facebook.com/docs/tutorials/ios-sdk-tutorial/authenticate/
     
     //1. Login to Facebook
+    NSArray *permissions = [NSArray arrayWithObjects:@"read_stream", nil];
+    [FBSession openActiveSessionWithReadPermissions: permissions
+                                       allowLoginUI:YES
+                                  completionHandler: ^(FBSession *session, FBSessionState state, NSError *error) {
+         [self sessionStateChanged:session state:state error:error];
+     }];
     
-    [self loadMeal];
+
 }
 
 - (void)loadMeal
 {
-    //TODO: Implement Facebook Graph Request
-    
-    //Help: You need https://graph.facebook.com/ulf.hansen.73/feed?limit=1 with a valid access token (you should get it after login)
-    //You should only load the last meal, old meals are uninteresting, not?
-    
-    //Help (Maybe) (source: https://developers.facebook.com/docs/howtos/batch-requests-ios-sdk/ )
-    
-    /*
-     [FBRequestConnection startWithGraphPath:@"ulf.hansen.73/feed?limit=1" 
-                completionHandler:^(FBRequestConnection *connection, id result, NSError *error) 
-         {
-            if (!error) {
-     
-                //We don't want the whole text... extract everything behind the :
-     
-                // Set the menu
-                self.meal = [[[result objectForKey:@"data"]
-                    objectAtIndex:0]
-                    objectForKey:@"message"];
-     
-                [self performSegueWithIdentifier:@"menuViewSegue" sender:self];
+    [[FBRequest requestForGraphPath:@"/ulf.hansen73/feed?fields=message"] startWithCompletionHandler:
+     ^(FBRequestConnection *connection, id result, NSError *error) {
+         if (!error) {
+             self.meal = [[[result objectForKey:@"data"]
+                           objectAtIndex:0]
+                          objectForKey:@"message"];
+             NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"Tagesessen .*: " options:NSRegularExpressionCaseInsensitive error:&error];
+             
+             self.meal = [regex stringByReplacingMatchesInString:self.meal options:0 range:NSMakeRange(0, [self.meal length]) withTemplate:@""];
+             [self performSegueWithIdentifier:@"menuViewSegue" sender:self];
          }
-     }];
-     */
-    
-    /* remove this later!! */ self.meal = @"Bouletten mit Bohnen in Bechamelsauce, dazu Kartoffeln und Salat";
-    /* remove this later!! */ [self performSegueWithIdentifier:@"menuViewSegue" sender:self]; // remove this later
-
-    
+     } ];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -76,6 +66,39 @@
         // Pass any objects to the view controller here, like...
         vc.meal = self.meal;
     }
+}
+
+
+- (void)sessionStateChanged:(FBSession *)session
+                      state:(FBSessionState) state
+                      error:(NSError *)error
+{
+    switch (state) {
+        case FBSessionStateOpen: {
+            NSLog(@"Open");
+            [self loadMeal];
+            break;
+        }        
+        case FBSessionStateClosed:
+            NSLog(@"Closed");
+            break;
+        case FBSessionStateClosedLoginFailed:
+            NSLog(@"LoginFailed");
+            break;
+        default:
+            NSLog(@"Def.");
+            break;
+    }
+    
+    if (error) {
+        UIAlertView *alertView = [[UIAlertView alloc]
+                                  initWithTitle:@"Error"
+                                  message:error.localizedDescription
+                                  delegate:nil
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil];
+        [alertView show];
+    }    
 }
 
 @end
