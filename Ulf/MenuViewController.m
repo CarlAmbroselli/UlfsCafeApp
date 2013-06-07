32b92077a827c449b7bed00b2f686b014e0aa1b9
Dustin Gedlich <DasDing.GehtNicht@gmail.com>
2013-06-07 18:35:40 +0200

    download is now async. added checkmark

--- Ulf/MenuViewController.m
+++ Ulf/MenuViewController.m
@@ -33,48 +33,60 @@
     hud.mode = MBProgressHUDModeIndeterminate;
     hud.labelText = @"Wird geladen...";
     
-    NSData * jsonData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://openmensa.org/api/v2/canteens/112/meals"]];
-    if (jsonData != nil){
-        NSLog(@"Meal loaded");
-
-        
-        @try {
-            NSArray * arrayData = [NSJSONSerialization
-                                   JSONObjectWithData: jsonData
-                                   options: NSJSONReadingMutableContainers
-                                   error:nil];
-            
-            NSString * meal  = [[[[arrayData objectAtIndex:0] objectForKey:@"meals"] objectAtIndex:0] objectForKey:@"name"];
-            
-            NSNumber * price = [[[[[arrayData objectAtIndex:0] objectForKey:@"meals"] objectAtIndex:0] objectForKey:@"prices"] objectForKey:@"others"];
-            NSNumberFormatter * format = [[NSNumberFormatter alloc ] init];
-            [format setNumberStyle:NSNumberFormatterDecimalStyle];
-            [format setPositiveFormat: @"Preis: #.00€"];
-            
-            BOOL closed  = [[[arrayData objectAtIndex:0] objectForKey:@"closed"] boolValue];
-            
-            if(!closed){
-                self.menuLabel.text = meal;
-                self.priceLabel.text = [format stringFromNumber:price];
-            }
-            else{
-                self.menuLabel.text = @"Ulf's Cafe ist momentan leider geschlossen!";
+    // start asynchron download
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+        NSData * jsonData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://openmensa.org/api/v2/canteens/112/meals"]];
+        // callback routine
+        dispatch_async(dispatch_get_main_queue(), ^(void){
+            if (jsonData != nil){
+                NSLog(@"Meal loaded");
+                @try {
+                    NSArray * arrayData = [NSJSONSerialization
+                                           JSONObjectWithData: jsonData
+                                           options: NSJSONReadingMutableContainers
+                                           error:nil];
+                                      
+                    BOOL closed  = [[[arrayData objectAtIndex:0] objectForKey:@"closed"] boolValue];
+                    
+                    if(!closed){
+                        // parsing Data
+                        NSString * meal  = [[[[arrayData objectAtIndex:0] objectForKey:@"meals"] objectAtIndex:0] objectForKey:@"name"];
+                        NSNumber * price = [[[[[arrayData objectAtIndex:0] objectForKey:@"meals"] objectAtIndex:0] objectForKey:@"prices"] objectForKey:@"others"];
+                        // formating price
+                        NSNumberFormatter * format = [[NSNumberFormatter alloc ] init];
+                        [format setNumberStyle:NSNumberFormatterDecimalStyle];
+                        [format setPositiveFormat: @"Preis: #.00€"];
+                        // setting UILabels
+                        self.menuLabel.text = meal;
+                        self.priceLabel.text = [format stringFromNumber:price];
+                        hud.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]];
+                        hud.mode = MBProgressHUDModeCustomView;
+                        hud.labelText = @"";
+                        
+                        
+                    }
+                    else{
+                        // Ulf closed
+                        self.menuLabel.text = @"Ulf's Cafe ist momentan leider geschlossen!";
+                        self.priceLabel.text = @"";
+                    }
+                }
+                @catch (NSException *exception) {
+                    NSLog(@"FERIEN!");
+                    self.menuLabel.text = @"Momentan ist leider kein Tagesessen verfügbar.";
+                    self.priceLabel.text = @"";
+                }
+                
+                
+            } else {
+                NSLog(@"Meal could not be retrieved");
+                self.menuLabel.text = @"Tagesessen konnte nicht geladen werden.";
                 self.priceLabel.text = @"";
             }
-        }
-        @catch (NSException *exception) {
-            NSLog(@"FERIEN!");
-            self.menuLabel.text = @"Momentan ist leider kein Tagesessen verfügbar.";
-            self.priceLabel.text = @"";
-        }
-    
-        
-    } else {
-        NSLog(@"Meal could not be retrieved");
-        self.menuLabel.text = @"Tagesessen konnte nicht geladen werden.";
-        self.priceLabel.text = @"";
-    }
-    [hud hide:YES];
+            [hud hide:YES afterDelay:0.5];
+            
+        });
+    });
 }
 
 
