c0d3e98d005c4a11c8d3ee4a13f657cc531a0e2b
Dustin Gedlich <DasDing.GehtNicht@gmail.com>
2013-06-07 12:06:49 +0200

    Facebook not required. Using now OpenMensa API

--- Ulf/MenuViewController.m
+++ Ulf/MenuViewController.m
@@ -22,50 +22,28 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     NSLog(@"MenuViewDidAppear");
-    //    if FBSession is active load the meal
-    //  else change to loginView to get a new FBSession
-
-    if ( FBSession.activeSession.isOpen){
-        NSLog(@"try to load Meal");
-        [self loadMeal];
-    } else {
-        [self changeView];
-    }
+    [self loadMeal];
 }
 
 - (void)loadMeal
 {
-    [[FBRequest requestForGraphPath:@"/ulf.hansen73/feed?fields=message"] startWithCompletionHandler:
-     ^(FBRequestConnection *connection, id result, NSError *error) {
-         if (!error) {
-             if ([[[result objectForKey:@"data"] objectAtIndex:0] objectForKey:@"message"] == nil){
-//               Ulf ist not your Friend :(
-                 NSLog(@"Ulf is not your Friend");
-                 self.menuLabel.text = @"Du bist nicht mit Ulf befreundet.";
-             } else {
-//               Ulf is Friend :D
-             
-                 NSString * meal = [[[result objectForKey:@"data"]
-                           objectAtIndex:0]
-                          objectForKey:@"message"];
-                 NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"Tagesessen .*: " options:         NSRegularExpressionCaseInsensitive error:&error];
-             
-                 meal = [regex stringByReplacingMatchesInString: meal options:0 range:NSMakeRange(0, [meal length]) withTemplate:@""];
-                 self.menuLabel.text = meal;
-                NSLog(@"Meal loaded");
-             }
-             
-         } else {
-             [FBSession.activeSession closeAndClearTokenInformation];
-             NSLog(@"FBRequest failed");
-             [self changeView];
-         }
-     } ];
-}
+    NSData * jsonData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://openmensa.org/api/v2/canteens/112/meals"]];
+    if (jsonData != nil){
+    NSArray * arrayData = [NSJSONSerialization
+                 JSONObjectWithData: jsonData
+                 options: NSJSONReadingMutableContainers
+                 error:nil];
+    
+    NSString * meal  = [[[[arrayData objectAtIndex:0] objectForKey:@"meals"] objectAtIndex:0] objectForKey:@"name"];
+    id price = [[[[[arrayData objectAtIndex:0] objectForKey:@"meals"] objectAtIndex:0] objectForKey:@"prices"] objectForKey:@"others"];
+        
+        self.menuLabel.text = meal;
+    } else {
+        self.menuLabel.text = @"Tagesessen konnte nicht geladen werden.";
+    }
+    
 
 
-- (void)changeView {
-    [self performSegueWithIdentifier:@"loginViewSegue" sender:self];
 }
 
 
